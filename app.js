e09325c573acd1cde8348008c7aecf3b7c4f4471
Solace <treynolds4@student.yosemite.edu>
2017-09-01 06:16:34 -0700

    fixed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json")
 console.log('Connecting...');
 //Crash haltings
 process.on('uncaughtException', err => {
